db1cca587d34f6387f7f5b2f978ed8a670d28860
Azar <azar.wirth@outlook.com>
2025-07-25 08:25:56 +0200

    removed files

--- secret_cat_society/frontend/firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAEr33Xkv6MLFddOVEXE1C0YfH3X1Bgk-c",
-  authDomain: "secretcatsociety-4b605.firebaseapp.com",
-  projectId: "secretcatsociety-4b605",
-  storageBucket: "secretcatsociety-4b605.appspot.com",
-  messagingSenderId: "264416108947",
-  appId: "1:264416108947:web:4606190d070caecb6e4866"
-};
